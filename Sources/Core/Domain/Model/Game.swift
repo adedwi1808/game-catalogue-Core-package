2e527871496e6f1c429ca8dd4ce7fcc731027f01
Ade Dwi Prayitno <adedwip1808@gmail.com>
2026-01-17 01:22:13 +0700

    Expose public initializers for Game models

--- Sources/Core/Domain/Model/Game.swift
+++ Sources/Core/Domain/Model/Game.swift
@@ -17,15 +17,40 @@ public struct Game {
     public let description: String?
     public let added: Int?
     public let developers: String?
+    
+    public init(id: Int, name: String, released: String, backgroundImage: String?, rating: Double?, ratingCount: Int?, platforms: [PlatformElement], genres: [Genre], description: String?, added: Int?, developers: String?) {
+        self.id = id
+        self.name = name
+        self.released = released
+        self.backgroundImage = backgroundImage
+        self.rating = rating
+        self.ratingCount = ratingCount
+        self.platforms = platforms
+        self.genres = genres
+        self.description = description
+        self.added = added
+        self.developers = developers
+    }
 }
 
 public struct Genre {
     public let id: Int
     public let name: String
+    
+    public init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
 }
 
 public struct PlatformElement {
     public let id: Int
     public let name: String
     public let slug: String
+    
+    public init(id: Int, name: String, slug: String) {
+        self.id = id
+        self.name = name
+        self.slug = slug
+    }
 }
